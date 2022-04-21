44261ebc42c07a0d377f5cfee5c529b30cadb244
kupermanak <kupermanak@gmail.com>
2022-04-21 10:56:05 +0000

    login/register estatico

--- src/front/js/pages/Signup.js
+++ src/front/js/pages/Signup.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 export const Signup = () => {
+  const [email, setEmail] = React.useState("");
+  const [pass, setPass] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [document, setDocument] = React.useState("");
+  const [paypalLink, setPaypalLink] = React.useState("");
+
+  const [error, setError] = React.useState(null);
+
   return (
     <div className="mt-5">
       <h3 className="text-center">Sign Up</h3>
@@ -13,54 +24,64 @@ export const Signup = () => {
               type="email"
               className="form-control mb-2"
               placeholder="Prueba@prueba.com"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
             <label className="mb-2">Password</label>
             <input
               type="password"
               className="form-control mb-2"
               placeholder="******"
-            />
-            <label className="mb-2">Repeat Password</label>
-            <input
-              type="password"
-              className="form-control mb-2"
-              placeholder="******"
+              onChange={(e) => setPass(e.target.value)}
+              value={pass}
             />
             <label className="mb-2">First Name</label>
             <input
               type="text"
               className="form-control mb-2"
               placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
             />
             <label className="mb-2">Last Name</label>
             <input
               type="text"
               className="form-control mb-2"
               placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
             />
             <label className="mb-2">Address</label>
             <input
               type="text"
               className="form-control mb-2"
               placeholder="Address"
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
             />
             <label className="mb-2">Phone</label>
             <input
               type="text"
               className="form-control mb-2"
               placeholder="Phone"
+              onChange={(e) => setPhone(e.target.value)}
+              value={phone}
             />
             <label className="mb-2">N° Document</label>
             <input
               type="number"
               className="form-control mb-2"
               placeholder="N° Document"
+              onChange={(e) => setDocument(e.target.value)}
+              value={document}
             />
             <label className="mb-2">Paypal link</label>
             <input
               type="text"
               className="form-control mb-2"
               placeholder="Paypal Link"
+              onChange={(e) => setPaypalLink(e.target.value)}
+              value={paypalLink}
             />
             <div className="form-check mt-3">
               <input
