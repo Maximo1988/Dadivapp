4f9dbdc77c2b9b2e94ce4617457563ffd9e13da0
kupermanak <kupermanak@gmail.com>
2022-05-07 01:06:23 +0000

    map projectos

--- src/front/js/component/cardContainer.js
+++ src/front/js/component/cardContainer.js
@@ -3,20 +3,21 @@ import PropType from "prop-types";
 
 export const CardContainer = (props) => {
   const [statusCard, setStatusCard] = useState(false);
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
-  let toggleOpen = () => setToggle(!toggle)
-  const menuClass = `dropdown-menu${(toggle ? " show" : "")}`
+  let toggleOpen = () => setToggle(!toggle);
+  const menuClass = `dropdown-menu${toggle ? " show" : ""}`;
 
   return (
     <div>
+      <button>boton</button>
       <div className="card estilo border-info pb-3 ml-5 w-100">
-        <div className="card-header">{props.Header}</div>
+        <div className="card-header">{props.name}</div>
         <div className="card-body text-body">
-          <h5 className="card-subtitle mb-2 text-muted">{props.Info}</h5>
-          <p className="card-text">{props.Description}</p>
+          <h5 className="card-subtitle mb-2 text-muted">{props.date_finish}</h5>
+          <p className="card-text">{props.donative_amount}</p>
           {statusCard === true ? (
-            <p className="card-text">{props.extraDescription}</p>
+            <p className="card-text">{props.description}</p>
           ) : (
             ""
           )}
@@ -41,41 +42,100 @@ export const CardContainer = (props) => {
             ""
           )}
 
-         { props.notButtonDonate? "":<div className="dropdown myDropdown px-3" style={{ float: "right" }} onClick={() => toggleOpen()}>
-            <button
-              className="btn btn-info text-white dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false">
-              Donate
-            </button>
-            <div className={menuClass} aria-labelledby="dropdownMenuButton ">
-              <ul className="text-info bg-white">
-                <div className="custom-control custom-radio">
-                  <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"></input>
-                  <label className="custom-control-label" htmlFor="customRadio1">Donate $1000</label>
-                </div>
-                <div className="custom-control custom-radio">
-                  <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"></input>
-                  <label className="custom-control-label" htmlFor="customRadio1">Donate $1500</label>
-                </div>
-                <div className="custom-control custom-radio">
-                  <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"></input>
-                  <label className="custom-control-label"   htmlFor="customRadio1">Donate $2000</label>
-                </div>
-                <div className="custom-control custom-radio">
-                  <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"></input>
-                  <label className="custom-control-label" htmlFor="customRadio1">Donate $2500</label>
-                </div>
-                <div className="custom-control custom-radio">
-                  <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"></input>
-                  <label className="custom-control-label" htmlFor="customRadio1">Donate $2500</label>
-                </div>
-              </ul>
+          {props.notButtonDonate ? (
+            ""
+          ) : (
+            <div
+              className="dropdown myDropdown px-3"
+              style={{ float: "right" }}
+              onClick={() => toggleOpen()}
+            >
+              <button
+                className="btn btn-info text-white dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Donate
+              </button>
+              <div className={menuClass} aria-labelledby="dropdownMenuButton ">
+                <ul className="text-info bg-white">
+                  <div className="custom-control custom-radio">
+                    <input
+                      type="radio"
+                      id="customRadio1"
+                      name="customRadio"
+                      className="custom-control-input"
+                    ></input>
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio1"
+                    >
+                      Donate $1000
+                    </label>
+                  </div>
+                  <div className="custom-control custom-radio">
+                    <input
+                      type="radio"
+                      id="customRadio1"
+                      name="customRadio"
+                      className="custom-control-input"
+                    ></input>
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio1"
+                    >
+                      Donate $1500
+                    </label>
+                  </div>
+                  <div className="custom-control custom-radio">
+                    <input
+                      type="radio"
+                      id="customRadio1"
+                      name="customRadio"
+                      className="custom-control-input"
+                    ></input>
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio1"
+                    >
+                      Donate $2000
+                    </label>
+                  </div>
+                  <div className="custom-control custom-radio">
+                    <input
+                      type="radio"
+                      id="customRadio1"
+                      name="customRadio"
+                      className="custom-control-input"
+                    ></input>
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio1"
+                    >
+                      Donate $2500
+                    </label>
+                  </div>
+                  <div className="custom-control custom-radio">
+                    <input
+                      type="radio"
+                      id="customRadio1"
+                      name="customRadio"
+                      className="custom-control-input"
+                    ></input>
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customRadio1"
+                    >
+                      Donate $2500
+                    </label>
+                  </div>
+                </ul>
+              </div>
             </div>
-          </div>}
+          )}
         </div>
         <footer className="blockquote-footer">
           <cite title="Source Title">dd/mm/year</cite>
@@ -84,7 +144,7 @@ export const CardContainer = (props) => {
           <cite title="Source Title">dd/mm/year</cite>
         </footer>
       </div>
-    </div >
+    </div>
   );
 };
 
--- src/front/js/component/cardInfo.js
+++ src/front/js/component/cardInfo.js
@@ -1,47 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import CardContainer from "./cardContainer";
 
 const CardInfo = () => {
-  const comments = [
-    {
-      Header: "Nombre",
-      Info: "Nombre del proyecto",
-      Description: "Cuanto dinero necesita/resumen",
-      extraDescription: "Descripcion larga del proyecto",
-    },
-    {
-      Header: "Nombre",
-      Info: "Nombre del proyecto",
-      Description: "Cuanto dinero necesita/resumen",
-      extraDescription: "Descripcion larga del proyecto",
-    },
-    {
-      Header: "Nombre",
-      Info: "Nombre del proyecto",
-      Description: "Cuanto dinero necesita/resumen",
-      extraDescription: "Descripcion larga del proyecto",
-    },
-    {
-      Header: "Nombre",
-      Info: "Nombre del proyecto",
-      Description: "Cuanto dinero necesita/resumen",
-      extraDescription: "Descripcion larga del proyecto",
-    },
-  ];
+  const { store, actions } = useContext(Context);
+  // const comments = [
+  //   {
+  //     Header: "Nombre",
+  //     Info: "Nombre del proyecto",
+  //     Description: "Cuanto dinero necesita/resumen",
+  //     extraDescription: "Descripcion larga del proyecto",
+  //   },
+  //   {
+  //     Header: "Nombre",
+  //     Info: "Nombre del proyecto",
+  //     Description: "Cuanto dinero necesita/resumen",
+  //     extraDescription: "Descripcion larga del proyecto",
+  //   },
+  //   {
+  //     Header: "Nombre",
+  //     Info: "Nombre del proyecto",
+  //     Description: "Cuanto dinero necesita/resumen",
+  //     extraDescription: "Descripcion larga del proyecto",
+  //   },
+  //   {
+  //     Header: "Nombre",
+  //     Info: "Nombre del proyecto",
+  //     Description: "Cuanto dinero necesita/resumen",
+  //     extraDescription: "Descripcion larga del proyecto",
+  //   },
+  // ];
   return (
     <div className="container">
       <div className="row">
-        {comments.map((comment, index) => {
-          return (
-            <div key={index} className="col">
-              <CardContainer index={index}
-                Header={comment.Header}
-                Info={comment.Info}
-                Description={comment.Description}
-                extraDescription={comment.extraDescription}
-              />{" "}
-            </div>
-          );
+        {store.projects_all.map((project) => {
+          <div key={project.name} className="col">
+            <CardContainer
+              nombre={project.name}
+              Fecha_inicio={project.date_start}
+              Fecha_final={project.date_finish}
+              Descripcion={project.description}
+              Monto_donado={project.donative_amount}
+            />
+          </div>;
         })}
       </div>
     </div>
